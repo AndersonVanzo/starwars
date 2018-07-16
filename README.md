d0d4ca49bcfc98490a74bb95a318a2964a092009
Anderson Vanzo <37242076+AndersonVanzo@users.noreply.github.com>
2018-07-15 21:35:06 -0300

    Adição dos arquivos

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Star-Wars
-Uma página sobre Star Wars (feita em inglês) para um trabalho da disciplina de Desenvolvimento de Sistemas para Dispositivos Móveis 1
+# MediaQuery
---
+++ js/js.js
@@ -0,0 +1,140 @@
+$(".btn-menu").click(function() {
+   $(".menu").show(); 
+});
+
+$(".btn-close").click(function() {
+    $(".menu").hide();
+});
+
+//---------------------------------
+
+$("#i").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").show();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#ii").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").show();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#iii").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").show();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#iv").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").show();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#v").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").show();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#vi").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").show();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#vii").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").show();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#viii").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").show();
+	$("#ri-div").hide();
+	$("#hs-div").hide();
+});
+$("#ri").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").show();
+	$("#hs-div").hide();
+});
+$("#hs").click(function() {
+	$("#tt-div").hide();
+	$("#i-div").hide();
+	$("#ii-div").hide();
+	$("#iii-div").hide();
+	$("#iv-div").hide();
+	$("#v-div").hide();
+	$("#vi-div").hide();
+	$("#vii-div").hide();
+	$("#viii-div").hide();
+	$("#ri-div").hide();
+	$("#hs-div").show();
+});
